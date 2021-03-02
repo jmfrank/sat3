94f2ef27122065f4fb04e753bbe43f804daed513
JMF <j.matt.franklin@gmail.com>
2021-03-01 17:00:53 -0800

    update

--- u2os_super_enhancers/retrieve_gprch_coordinates.py
+++ u2os_super_enhancers/retrieve_gprch_coordinates.py
@@ -9,7 +9,6 @@ data = pd.read_csv(data_file,sep=',',header=None)
 data.columns=['chr','start','stop']
 
 genome=gen("/media/ngs/data/genomes/GRCh38.p13/", "GCF_000001405.39_GRCh38.p13_genomic.fna")
-
-
 fasta = genome.get_genome_pysam()
 
+for index, row in data.itterows():
